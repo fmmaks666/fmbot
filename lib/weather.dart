701ce08ed412e28c4458258572b03a748e7209ee
fmmaks <108734737+fmmaks666@users.noreply.github.com>
2024-10-02 20:45:09 +0300

    Fixed Weather parser

--- lib/weather.dart
+++ lib/weather.dart
@@ -5,7 +5,7 @@ import 'package:html/dom.dart' show Element;
 class Weather {
   // TODO: Support weather icon (emoji in this case)
   final String month;
-  final int day;
+  final String day;
   final String info;
   final String temperatureMin; // Would be nice to store it as int
   final String temperatureMax; // ^
@@ -20,7 +20,17 @@ class Weather {
 
 class SinoptikParser {
   final Client client;
+	static const tabsClass = ".D5LKqju5";
+	static const monthClass = ".CAXTD7\\+X";
+	static const dayClass = ".BrJ0wZrO";
+	static const weekDayClass = ".BzO81ZRx";
+	static const weatherIcoClass = ".EAadAKAr"; // Not sure whether this is the correct class
+	static const minClass = ".XyT+Rm+n";
+	static const maxClass = ".cFBF0wTW";
+	static const temperatureLabelClass = "._4skXjqhc";
+	static const temperaturesQuery = ".cFBF0wTW p:not(._4skXjqhc)";
 
+	// Add Custom UserAgent
   SinoptikParser() : client = Client();
 
   Future<List<Weather>> parseWeather(Uri url) async {
@@ -40,25 +50,26 @@ class SinoptikParser {
     List<Weather> reports = [];
     final HtmlParser parser = HtmlParser(page);
     final document = parser.parse();
-    final tabs = document.querySelector(".tabs")?.children;
+    final tabs = document.querySelector(tabsClass)?.children;
     if (tabs == null) return <Weather>[];
     for (Element tab in tabs) {
-      if (!tab.classes.contains("main")) continue;
+      //if (!tab.classes.contains("main")) continue;
       reports.add(_parseCard(tab));
     }
     return reports;
   }
 
   Weather _parseCard(final Element tab) {
-    final month = tab.querySelector(".month")?.text ?? "???";
-    final day = int.tryParse(tab.querySelector(".date")?.text ?? "") ?? -1;
-    final info = tab.querySelector(".weatherIco")?.attributes["title"] ??
+    final month = tab.querySelector(monthClass)?.text ?? "???";
+    final day = int.tryParse(tab.querySelector(dayClass)?.text ?? "") ?? -1;
+		final weekday = tab.querySelector(weekDayClass)?.innerHtml;
+    final info = tab.querySelector(weatherIcoClass)?.attributes["aria-label"] ??
         "???"; // May fail (accessing title attribute)
-    final tempMin =
-        tab.querySelector(".min")?.querySelector("span")?.text ?? "0";
-    final tempMax =
-        tab.querySelector(".max")?.querySelector("span")?.text ?? "0";
-    return Weather(month, day, info, tempMin, tempMax);
+		final temps = tab.querySelectorAll(temperaturesQuery);
+    final tempMin = temps[0].innerHtml;
+    final tempMax = temps[1].innerHtml;
+		final fullDay = "$weekday, $day";
+   	return Weather(month, fullDay, info, tempMin, tempMax);
   }
 
   void shutdown() {
